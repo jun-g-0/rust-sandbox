439d7249224a8cb2310a5f2a72aaa2aa304a9c5f
Jun Gao <junworks7@gmail.com>
2021-04-08 11:59:01 +0900

    feat: serve static files

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder, middleware};
+use actix_files::Files;
 
-#[get("/")]
+#[get("/hello")]
 async fn hello() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
 }
@@ -16,10 +17,16 @@ async fn manual_hello() -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    std::env::set_var("RUST_LOG", "actix_web=info");
+    env_logger::init();
+
     HttpServer::new(|| {
         App::new()
+            .wrap(middleware::Logger::default())
             .service(hello)
             .service(echo)
+            .service(Files::new("/images", "static/images/").show_files_listing())
+            .service(Files::new("/", "./static").index_file("index.html"))
             .route("/hey", web::get().to(manual_hello))
     })
     .bind("127.0.0.1:8080")?
